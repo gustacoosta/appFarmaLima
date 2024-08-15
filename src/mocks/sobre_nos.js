6a7142c49d2045acc00cd04af2f407ec40381701
Gustavo Lima Costa <costaagustavo9@gmail.com>
2024-08-14 21:10:31 -0300

    Update sobre_nos.js

--- src/mocks/sobre_nos.js
+++ src/mocks/sobre_nos.js
@@ -6,6 +6,7 @@ const sobre_nos = {
         logo: LogoFarma,
         historia: "Desde 2024, a Farma Lima tem sido sua parceira confiável em saúde e bem-estar. Nosso compromisso é oferecer produtos farmacêuticos de alta qualidade e serviços excepcionais. Com uma equipe dedicada e treinada, garantimos a autenticidade e a segurança de todos os produtos que oferecemos. Além disso, somos ativos em apoiar iniciativas locais de saúde e bem-estar. Escolha a Farma Lima como sua farmácia de confiança e junte-se a nós em sua jornada rumo à saúde e ao bem-estar.",
         imagem: Imagem,
+        intro_vid: "Você é muito especial para nós, e preparamos uma experiência única com todo o carinho, pensando em cada detalhe para superar suas expectativas!",
     }
 }
 
